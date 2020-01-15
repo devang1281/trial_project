555a3c29311d74016cf19cf8b3ced12ae7d41bc6
devang <devang.b@talview.com>
2020-01-15 12:11:08 +0530

    Initial Commit

--- temp.c
+++ temp.c
@@ -1,2 +1,14 @@
+//Multiplication in C
 #include<stdio.h>
-//Null File
+int multi(int num1,int num2)
+{
+  int pro = num1 * num2;
+  return pro;
+}
+
+void main()
+{
+  int product;
+  product = multi(5,10);
+}
+
